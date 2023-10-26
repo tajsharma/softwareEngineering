fa4da69f63491366cb5fee7c13f5fbb1ae2662b1
Taj <sharma.tajinder01@gmail.com>
2023-10-26 16:18:42 -0400

    Concrete interface using LBState

--- hw1/src/main/java/hwdp/OnShelf.java
+++ hw1/src/main/java/hwdp/OnShelf.java
@@ -1,2 +1,39 @@
 package hwdp;
-// TODO HWDP P2
+
+public class OnShelf implements LBState {
+    private static OnShelf instance;
+    private OnShelf() {}
+
+    public static OnShelf getInstance() {
+        if (instance == null) {
+            instance = new OnShelf();
+        }
+        return instance;
+    }
+
+    @Override
+    public void shelf(LibraryBook book) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void issue(LibraryBook book) {
+        LibraryLogger.getInstance().writeLine("Leaving State OnShelf for State Borrowed");
+        book.setState(Borrowed.getInstance());
+    }
+
+    @Override
+    public void extend(LibraryBook book) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void returnIt(LibraryBook book) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String toString() {
+        return "OnShelf";
+    }
+}
