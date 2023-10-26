7c6dbf7ccb1cb2c7cb5de2f300d09f5c9b82cfd9
Taj <sharma.tajinder01@gmail.com>
2023-10-26 16:17:06 -0400

    Add import for util List

--- hw1/src/main/java/hwdp/LibraryLogger.java
+++ hw1/src/main/java/hwdp/LibraryLogger.java
@@ -1,24 +1,35 @@
 package hwdp;
 import java.util.ArrayList;
+import java.util.List;
 
 public class LibraryLogger {
-    // TODO?
+    private static LibraryLogger instance = null;
+    private List<String> writtenLines = new ArrayList<>();
+
+    // private constructor
+    private LibraryLogger() {
+        ExpensiveComputeToy.performExpensiveLogSetup();
+    }
+
 
     public void writeLine(String line) {
-        // TODO?
+        System.out.println("libraryLogger: " + line);
+        writtenLines.add(line);
     }
 
     public String[] getWrittenLines() {
-        // TODO?
-        return null;
+        return writtenLines.toArray(new String[0]);
     }
 
     public void clearWriteLog() {
-        // TODO?
+        writtenLines.clear();
     }
-    
+
     public static LibraryLogger getInstance() {
-        // TODO?
-        return null;
+        if (instance == null) {
+            instance = new LibraryLogger();
+        }
+        return instance;
     }
 }
+9
