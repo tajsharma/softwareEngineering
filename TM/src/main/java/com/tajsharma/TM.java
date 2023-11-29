46db9ef31ade443f229e1d58341d104c9117ba49
tajsharma <sharma.tajinder01@gmail.com>
2023-11-29 14:55:40 -0800

    add switch first 2 switch statements to it to make sure it works; it does

--- TM/src/main/java/com/tajsharma/TM.java
+++ TM/src/main/java/com/tajsharma/TM.java
@@ -2,6 +2,30 @@ package com.tajsharma;
 
 public class TM {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if(args.length ==0){
+            System.out.println("no command entered");
+            return;
+        }
+        String command = args[0].toLowerCase();
+
+        switch(command){
+            case "start":
+                startTask(args);
+                break;
+            case "stop":
+                stopTask(args);
+                break;
+        }
     }
+
+    private static void startTask(String[] args){
+        System.out.println("Entered start task");
+        return;
+    }
+
+    private static void stopTask(String[] args){
+        System.out.println("Entered Stop task");
+    }
+
+
 }
