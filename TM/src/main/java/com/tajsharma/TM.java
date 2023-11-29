b003b2d5d5a4ba271126b01e9779cc287f4d27ca
tajsharma <sharma.tajinder01@gmail.com>
2023-11-29 15:25:18 -0800

    Add switch statement for the rest of the commands as well as a method for each:

--- TM/src/main/java/com/tajsharma/TM.java
+++ TM/src/main/java/com/tajsharma/TM.java
@@ -15,6 +15,21 @@ public class TM {
             case "stop":
                 stopTask(args);
                 break;
+            case "describe":
+                describeTask(args);
+                break;
+            case "size":
+                defineSizeTask(args);
+                break;
+            case "rename":
+                renameTask(args);
+                break;
+            case "delete":
+                deleteTask(args);
+                break;
+            case "summary":
+                summaryOfTask(args);
+                break;
         }
     }
 
@@ -25,6 +40,26 @@ public class TM {
 
     private static void stopTask(String[] args){
         System.out.println("Entered Stop task");
+    }
+
+    private static void describeTask(String[] args){
+        System.out.println("Entered Stop task");
+    }
+
+    private static void defineSizeTask(String[] args){
+        System.out.println("Entered size task");
+    }
+
+    private static void renameTask(String[] args){
+        System.out.println("Entered rename task");
+    }
+
+    private static void deleteTask(String[] args){
+        System.out.println("Entered delete task");
+    }
+
+    private static void summaryOfTask(String[] args){
+        System.out.println("Entered summary task");
     }
 
 
